78237dd309d7f9d41455c1a658c2a780e658db2e
Harishoffl <r.harishku06@gmail.com>
2024-07-02 23:01:15 +0530

    added color code

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,16 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+    heading_color:"#f7ebdc",
+    text_color:"#e8c396",
+    bg_color:"#231608",
+    hover_colorL:"#ae7026",
+    card_bg_color:"#694317",
+    btn_color:"#d99b51",
+    btn_hover_color:"#e8c396",
+    btn_text_color:"#694317",
+    bg_color:"#231608",}
   },
   plugins: [],
 };
