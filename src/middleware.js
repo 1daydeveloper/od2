cb2f9c93f9e9ddca82ce4b1e31701f61d2aa9c85
1daydeveloper <120658537+1daydeveloper@users.noreply.github.com>
2024-12-04 23:17:37 +0530

    Issue so revering the middleware changes

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,20 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-export function middleware(request) {
-  const hostname = request.headers.get('host'); // e.g., 'tem.od2.in'
-  const url = request.nextUrl;
+// export function middleware(request) {
+//   const hostname = request.headers.get('host'); // e.g., 'tem.od2.in'
+//   const url = request.nextUrl;
 
-  // Extract subdomain
-  const [subdomain] = hostname.split('.'); // "tem" in "tem.od2.in"
-  const mainDomain = 'od2.in'; // Update this to your domain
+//   // Extract subdomain
+//   const [subdomain] = hostname.split('.'); // "tem" in "tem.od2.in"
+//   const mainDomain = 'od2.in'; // Update this to your domain
 
-  // Check if the request is for the "tem" subdomain
-  if (hostname !== mainDomain && subdomain === 'temp') {
-    // Keep assets and layouts intact while serving the page
-    url.pathname = `/tmail${url.pathname}`;
-    return NextResponse.rewrite(url);
-  }
+//   // Check if the request is for the "tem" subdomain
+//   if (hostname !== mainDomain && subdomain === 'temp') {
+//     // Keep assets and layouts intact while serving the page
+//     url.pathname = `/tmail${url.pathname}`;
+//     return NextResponse.rewrite(url);
+//   }
 
-  // Allow other requests to proceed
-  return NextResponse.next();
-}
+//   // Allow other requests to proceed
+//   return NextResponse.next();
+// }
