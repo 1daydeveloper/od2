1ac45a210070b931b7e4542ebb27c1b02176297d
1daydeveloper <120658537+1daydeveloper@users.noreply.github.com>
2024-12-04 23:01:52 +0530

    test middleware

--- src/middleware.js
+++ src/middleware.js
@@ -2,10 +2,8 @@ import { NextResponse } from 'next/server'
  
 export function middleware(request) {
   if (request.nextUrl.pathname.startsWith('/about')) {
-    return NextResponse.rewrite(new URL('/about-2', request.url))
+    return NextResponse.rewrite(new URL('/tmail', request.url))
   }
  
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.rewrite(new URL('/dashboard/user', request.url))
-  }
+  
 }
