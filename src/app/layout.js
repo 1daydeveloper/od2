0e5e8058d7314bf09f28843a621d2a5600ce6c21
1daydeveloper <120658537+1daydeveloper@users.noreply.github.com>
2024-12-20 17:47:03 +0530

    Added Gtag

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,6 +35,12 @@ export default function RootLayout({ children }) {
           gtag('config', 'G-1CC0XPGF77');
         `}
         </Script>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K3JKX9NM');</script>
+<!-- End Google Tag Manager -->
 
         <script
           async
@@ -58,6 +64,10 @@ export default function RootLayout({ children }) {
           theme="dark"
         />
         <Footer/>
+
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3JKX9NM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
       </body>
     </html>
   );
