de714e7770a891b4d6bae78783ad6105718f699b
Pradeep Kumar(developer) <onedaydevelopers@gmail.com>
2025-07-23 19:47:12 +0530

    fix bug and removed un used api

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,7 +1,13 @@
 import { getSortedPostsData } from "@main/lib/posts";
 import BlogListClient from "./BlogListClient";
+import BlogListSkeleton from "@/components/ui/blog-list-skeleton";
+import { Suspense } from "react";
 
 export default function BlogPage() {
   const allPostsData = getSortedPostsData();
-  return <BlogListClient allPostsData={allPostsData} />;
+  return (
+    <Suspense fallback={<BlogListSkeleton />}>
+      <BlogListClient allPostsData={allPostsData} />
+    </Suspense>
+  );
 }
